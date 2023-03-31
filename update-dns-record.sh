f757c9af2457097fcdc03bc5db7d104773e2b94c
anand nandhu <anandnandhu52@gmail.com>
2023-03-31 19:18:22 +0530

    jenkinscode

---
+++ update-dns-record.sh
@@ -0,0 +1,15 @@
+IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=jenkins"  --query 'Reservations[*].Instances[*].PublicIpAddress' --output text)
+
+echo '
+{
+ "Comment": "CREATE/DELETE/UPSERT a record ",
+  "Changes": [{
+    "Action": "UPSERT",
+    "ResourceRecordSet": {
+      "Name": "jenkins.anandnandhu.online",
+      "Type": "A",
+      "TTL": 15,
+      "ResourceRecords": [{ "Value": "IPADDRESS"}]
+    }}]
+
+}' | sed -e "s/IPADDRESS/${IP}/"
